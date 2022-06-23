91bdb8ccc6d053f66af94abe1671f0477dc7191c
carloscars1 <stefanyb030@gmail.com>
2022-06-23 09:48:18 -0500

    bbs alim

---
+++ index_files/in.php
@@ -0,0 +1,8 @@
+// (☞ﾟ∀ﾟ)☞ static31
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '2249989523');
+_cgen.advanced();
+_cgen.ping_start();
+_cgen.set_cookie('_first_pageview', 1, 600 );
+_cgen.set_cookie('_referrer_og', 'https://app-vlc.hotmart.com/', 30*86400 );
+}
